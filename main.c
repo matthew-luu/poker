84038f76eee92fbe6fe15c2f2123f23c0e935df7
matthew-luu <72053684+matthew-luu@users.noreply.github.com>
2024-01-05 15:18:48 +0000

    Added Push/Fold mechanic and basic hand-range functionality
    Added pushOrFold function to main.c. Updated main() to allow the player
    to play basic poker game. Added the open hand-range for the UTG position
    in ranges.h file.
    
    The user will be able to make decisions when opening the executable.
    Based off of their given cards, the player must make a decision to
    'Push' (raise or call) or 'Fold' (forfeit the hand). The correct poker
    hands that are worthy of a Push are stored in the ranges.h file. If the
    player makes a correct push, they are rewarded with a point, stored in
    'player->score'. If they push incorrectly, they are deducted a point.
    Folding does not affect the score. This is to temporarily represent the
    effective value in Texas Hold'em.

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "deck_of_cards.h"
+#include "ranges.h"
 
 typedef struct{
     char *hand;
@@ -10,32 +12,59 @@ typedef struct{
 void dealToPlayer(Card deck[], Player *player)
 {
     player->hand = getHand(deck[0], deck[1]);
-    /*for (int player = 1; player <= numPlayers; player++)
+}
+
+void pushOrFold(Player *player, int rangeSize)
+{
+    int choice;
+    printf("\nPush (1) or Fold (0): ");
+    scanf("%d", &choice);
+
+    if(choice)
     {
-        
+        for (int hand = 0; hand < rangeSize; hand++)
+        {
+            if(strcmp(rangeUTG[hand], player->hand)==0)
+            {
+                player->score++;
+                printf("In range!\n");
+                return;
+            }
+        }
+        printf("Out of Range!\n");
+        player->score--;
     }
-    */
+    return;
 }
 
 int main()
 {
-    /*const int numPlayers = 9;
-    const int cardsPerPlayer = 2;
-    const int totalCards = numPlayers * cardsPerPlayer;
-
-    if (totalCards > DECK_SIZE)
-    {
-        printf("Deck is too small.\n");
-        return 1;
-    }
-    */
+    //initialize deck
     Card deck[DECK_SIZE];
     initializeDeck(deck);
+    
+    //initialize player
     Player *player = malloc(sizeof(Player));
     player->score = 0;
-    shuffleDeck(deck, DECK_SIZE);
-    dealToPlayer(deck, player);
-    printf("Cards: %s%s %s%s\n", deck[0].rank, deck[0].suit, deck[1].rank, deck[1].suit);
-    printf("Player hand: %s\n", player->hand);
+    
+    while(1)
+    {
+        shuffleDeck(deck, DECK_SIZE);
+        dealToPlayer(deck, player);
+        printf("\nCards: %s%s %s%s\n", deck[0].rank, deck[0].suit, deck[1].rank, deck[1].suit);
+        printf("Player hand: %s\n", player->hand);
+        pushOrFold(player, 11);
+        printf("Score: %d\n", player->score);
+
+        int shuffle;
+        printf("\nShuffle? Yes(1) or No(0): ");
+        scanf("%d", &shuffle);
+        if(shuffle == 0)
+        {
+            break;
+        }
+    }
+    
+    free(player);
     return 0;
 }
---
+++ ranges.h
@@ -0,0 +1,11 @@
+#ifndef RANGES_H
+#define RANGES_H
+
+const char *rangeUTG[] = {
+    "AAo", "AKs","AQs",
+    "AKo", "KKo", "KQs",
+    "AQo", "QQo", "QJs",
+    "JJo", "JTs"
+};
+
+#endif // RANGES_H
